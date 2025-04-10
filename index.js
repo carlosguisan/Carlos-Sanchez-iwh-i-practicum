0730155d92792e5f35599d887fb08289954fa90e
Carlos Sanchez <carlosguisan@gmail.com>
2025-04-10 09:21:24 -0600

    Ready Step 1: Added home route and your view

--- index.js
+++ index.js
@@ -13,22 +13,22 @@ const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS_TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
+// * Code for Route 1 goes here
+
 app.get('/', async (req, res) => {
-   const getEvents = "https://api.hubapi.com/crm/v3/objects/2-43162702";
+   const getEvents = "https://api.hubapi.com/crm/v3/objects/2-43162702?properties=name,short_description,event_date";
    const headers = {
     Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
     'Content-Type': 'application/json'
    }
    try {
     const response = await axios.get(getEvents, { headers });
-    const data = response.data;
-    res.json(data);
+    const data = response.data.results;
+    res.render('Home', { title: 'Events | HubSpot APIs', data });
    } catch (error) {
     console.error(error);
    }
 })
-
-// * Code for Route 1 goes here
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
